c87164123f3f559848c3741fa59aad75d88d0c35
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-05-19 21:07:58 -0300

    Criado interface icadastro

---
+++ MusicFlow/MusicFlow/Models/ICadastro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicFlow.Models
+{
+    public enum Status
+    {
+        Ativo = 0, Inativo = 1
+    }
+    internal interface ICadastro
+    {
+        int? Id { get; set; }
+        string Nome { get; set; }
+        DateTime DataCadastro { get; set; }
+        Status Status { get; set; }
+    }
+}
